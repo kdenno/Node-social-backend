1eb04e77900cffee9f625a9d63850dfcdd4eb22f
kdenno <kabuyedennis3@gmail.com>
2020-03-10 19:28:15 +0300

    add auth middleware to all routes

--- routes/feed.js
+++ routes/feed.js
@@ -2,11 +2,12 @@ const express = require("express");
 const { body } = require("express-validator/check");
 
 const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
@@ -22,9 +23,10 @@ router.post(
   feedController.createPost
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 router.put(
   "/post/:postId",
+  isAuth,
   [
     body("title")
       .trim()
@@ -36,6 +38,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete("post/:postId", feedController.deletePost);
+router.delete("post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
